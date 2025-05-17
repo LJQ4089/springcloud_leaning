8e61b5871836e8bb89814827885d726ba527ced0
LJQ4089 <ljq4089@gmail.com>
2025-05-17 22:38:12 +0800

    20250517-test

---
+++ springcloud-config-server-3344/src/main/java/com/lijianqing/springcloud/Config_server_3344.java
@@ -0,0 +1,14 @@
+package com.lijianqing.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer // 开启spring cloud config server服务
+@SpringBootApplication
+public class Config_server_3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_server_3344.class,args);
+    }
+}
--- springcloud-provider-dept-hystrix-8001/src/main/java/com/lijianqing/springcloud/DeptProvider_Hystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/lijianqing/springcloud/DeptProvider_Hystrix_8001.java
@@ -1,10 +1,13 @@
 package com.lijianqing.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient//在服务启动后自动注册到eureka中
@@ -13,5 +16,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class DeptProvider_Hystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_Hystrix_8001.class, args);
+    }
+
+    //增加一个 Servlet
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        //访问该页面就是监控页面
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+
+        return registrationBean;
     }
 }
